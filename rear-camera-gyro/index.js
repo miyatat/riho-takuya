727d1217eb6269dea00746e6f411cc22b0100f9a
tmiyata <makestein.m@gmail.com>
2020-02-25 19:06:54 +0900

    push for deploy

--- rear-camera-gyro/index.js
+++ rear-camera-gyro/index.js
@@ -9,17 +9,6 @@ const medias = {
 const video = document.getElementById("video");
 const promise = navigator.mediaDevices.getUserMedia(medias);
 
-let sensor = new Gyroscope();
-sensor.start();
-
-sensor.onreading = () => {
-    console.log("Angular velocity around the X-axis " + sensor.x);
-    console.log("Angular velocity around the Y-axis " + sensor.y);
-    console.log("Angular velocity around the Z-axis " + sensor.z);
-};
-
-sensor.onerror = event => console.log(event.error.name, event.error.message);
-
 promise.then(successCallback)
        .catch(errorCallback);
 
